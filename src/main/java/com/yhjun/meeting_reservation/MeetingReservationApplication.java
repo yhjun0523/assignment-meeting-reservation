8700fe0697dba6c9121cedbcdae3bde72b6d5747
yhjun0523 <windofwish@gmail.com>
2025-05-11 17:19:36 +0900

    initial commit

---
+++ src/main/java/com/yhjun/meeting_reservation/MeetingReservationApplication.java
@@ -0,0 +1,13 @@
+package com.yhjun.meeting_reservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetingReservationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yhjun/meeting_reservation/MeetingReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yhjun.meeting_reservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
