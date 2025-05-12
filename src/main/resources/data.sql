b0583203ddd7e7dd63719f96dc6c364769da9b97
yhjun0523 <windofwish@gmail.com>
2025-05-12 18:50:15 +0900

    chore: DB 초기 데이터 설정 파일 추가

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO app_user (id, username, email)
+VALUES (1, '양현준', 'yhjun@example.com'),
+       (2, '홍길동', 'gdhong@example.com');
+INSERT INTO meeting_rooms (id, name)
+VALUES (1, '회의실 A');
